dfacd3519454b81b5450d2da4b989293b2c332be
Claus Hammann <mail@chammann.dev>
2025-04-28 11:47:17 +0200

    add protocol reaction and test

--- vsum/src/test/java/tools/vitruv/methodologisttemplate/vsum/VSUMExampleTest.java
+++ vsum/src/test/java/tools/vitruv/methodologisttemplate/vsum/VSUMExampleTest.java
@@ -42,20 +42,95 @@ public class VSUMExampleTest {
     VirtualModel vsum = createDefaultVirtualModel(tempDir);
     addUncertaintyAnnotationRepository(vsum, tempDir);
     // assert that the directly added System is present
-    Assertions.assertEquals(1,
-        getDefaultView(vsum, List.of(UncertaintyAnnotationRepository.class)).getRootObjects().size());
+    Assertions.assertEquals(1, getDefaultView(vsum, List.of(System.class)).getRootObjects().size());
     // as well as the Root that should be created by the Reactions, see
     // templateReactions.reactions#14
-    Assertions.assertEquals(1, getDefaultView(vsum, List.of(Brakesystem.class)).getRootObjects().size());
+    Assertions.assertEquals(1, getDefaultView(vsum, List.of(Root.class)).getRootObjects().size());
   }
 
-  private void addUncertaintyAnnotationRepository(VirtualModel vsum, Path projectPath) {
-    CommittableView view = getDefaultView(vsum, List.of(UncertaintyAnnotationRepository.class))
-        .withChangeDerivingTrait();
+  @Test
+  void insertComponent(@TempDir Path tempDir) {
+    InternalVirtualModel vsum = createDefaultVirtualModel(tempDir);
+    addSystem(vsum, tempDir);
+    addComponent(vsum);
+    Assertions.assertTrue(assertView(getDefaultView(vsum, List.of(System.class, Root.class)), (View v) -> {
+      // assert that a component has been inserted, a entity has been created and that
+      // both have the same name
+      // Note: to make the test result easier to understand, these different effects
+      // should be tested one by one
+      return v.getRootObjects(System.class).iterator().next()
+          .getComponents().get(0).getName()
+          .equals(v.getRootObjects(Root.class).iterator().next()
+              .getEntities().get(0).getName());
+    }));
+  }
+
+  @Test
+  void renameComponent(@TempDir Path tempDir) {
+    final String newName = "newName";
+    VirtualModel vsum = createDefaultVirtualModel(tempDir);
+    addSystem(vsum, tempDir);
+    addComponent(vsum);
+    modifyView(getDefaultView(vsum, List.of(System.class)).withChangeDerivingTrait(), (CommittableView v) -> {
+      // change the name of the component
+      v.getRootObjects(System.class).iterator().next().getComponents().get(0).setName(newName);
+    });
+    Assertions.assertTrue(assertView(getDefaultView(vsum, List.of(System.class, Root.class)), (View v) -> {
+      // assert that the renaming worked on the component as well as the corresponding
+      // entity
+      return v.getRootObjects(System.class).iterator().next()
+          .getComponents().get(0).getName().equals(newName)
+          && v.getRootObjects(Root.class).iterator().next()
+              .getEntities().get(0).getName().equals(newName);
+    }));
+  }
+
+  @Test
+  void deleteComponent(@TempDir Path tempDir) {
+    VirtualModel vsum = createDefaultVirtualModel(tempDir);
+    addSystem(vsum, tempDir);
+    addComponent(vsum);
+    modifyView(getDefaultView(vsum, List.of(System.class)).withChangeDerivingTrait(), (CommittableView v) -> {
+      v.getRootObjects(System.class).iterator().next().getComponents().remove(0);
+    });
+    Assertions.assertTrue(assertView(getDefaultView(vsum, List.of(System.class, Root.class)), (View v) -> {
+      // assert that the deletion of the component worked and that the corresponding
+      // entity also got deleted
+      return v.getRootObjects(System.class).iterator().next().getComponents().isEmpty()
+          && v.getRootObjects(Root.class).iterator().next().getEntities().isEmpty();
+    }));
+  }
+
+  @Test
+  void insertProtocol(@TempDir Path tempDir) {
+    InternalVirtualModel vsum = createDefaultVirtualModel(tempDir);
+    addSystem(vsum, tempDir);
+    addProtocol(vsum);
+    Assertions.assertTrue(assertView(getDefaultView(vsum, List.of(System.class, Root.class)), (View v) -> {
+      // assert that a protocol has been inserted, a entity has been created and that
+      // both have the same name
+      return v.getRootObjects(System.class).iterator().next()
+          .getProtocols().get(0).getName()
+          .equals(v.getRootObjects(Root.class).iterator().next()
+              .getEntities().get(0).getName());
+    }));
+  }
+
+  private void addSystem(VirtualModel vsum, Path projectPath) {
+    CommittableView view = getDefaultView(vsum, List.of(System.class)).withChangeDerivingTrait();
     modifyView(view, (CommittableView v) -> {
       v.registerRoot(
           UncertaintyFactory.eINSTANCE.createUncertaintyAnnotationRepository(),
           URI.createFileURI(projectPath.toString() + "/example.model"));
+    });
+  }
+
+  private void addProtocol(VirtualModel vsum) {
+    CommittableView view = getDefaultView(vsum, List.of(System.class)).withChangeDerivingTrait();
+    modifyView(view, (CommittableView v) -> {
+      var protocol = ModelFactory.eINSTANCE.createProtocol();
+      protocol.setName("specialname");
+      v.getRootObjects(System.class).iterator().next().getProtocols().add(protocol);
     });
   }
 
