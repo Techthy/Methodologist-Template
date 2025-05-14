e8e7c331df6fd2b3eabd165c7b6ab258a29f5efd
Nathan Hagel <nathan@hagel.dev>
2025-05-15 01:23:16 +0200

    Add test if same uncertainty already exists, no new uncertainty is added again automatically.

--- vsum/src/test/java/tools/vitruv/methodologisttemplate/vsum/VSUMExampleTest.java
+++ vsum/src/test/java/tools/vitruv/methodologisttemplate/vsum/VSUMExampleTest.java
@@ -275,7 +275,7 @@ public class VSUMExampleTest {
 
       var uncertainty = UncertaintyFactory.eINSTANCE.createUncertainty();
       uncertainty.setUncertaintyLocation(uncertaintyLocation);
-      uncertainty.setKind(UncertaintyKind.MEASUREMENT_UNCERTAINTY);
+      uncertainty.setKind(UncertaintyKind.BELIEF_UNCERTAINTY);
       uncertainty.setReducability(ReducabilityLevel.UNKNOWN);
       uncertainty.setNature(UncertaintyNature.ALEATORY);
 
@@ -289,16 +289,19 @@ public class VSUMExampleTest {
     // uncertainties
     Assertions.assertTrue(assertView(getDefaultView(vsum, List.of(UncertaintyAnnotationRepository.class)), (View v) -> {
       var uncertainties = v.getRootObjects(UncertaintyAnnotationRepository.class).iterator().next().getUncertainties();
+      System.out.println("Number of Uncertainties: " + uncertainties.size());
 
       long brakeDiskUncertainties = uncertainties.stream()
           .filter(u -> u.getUncertaintyLocation().getReferencesComponents().stream()
               .anyMatch(c -> c instanceof BrakeDisk && ((BrakeDisk) c).getDiameterInMM() == 120))
           .count();
+      System.out.println("brakeDiskUncertainties: " + brakeDiskUncertainties);
 
       long circleUncertainties = uncertainties.stream()
           .filter(u -> u.getUncertaintyLocation().getReferencesComponents().stream()
               .anyMatch(c -> c instanceof Circle && ((Circle) c).getRadius() == 60))
           .count();
+      System.out.println("circleUncertainties: " + circleUncertainties);
 
       boolean specsPresent = uncertainties.stream()
           .filter(u -> u.getUncertaintyLocation().getReferencesComponents().stream()
@@ -309,6 +312,102 @@ public class VSUMExampleTest {
           .containsAll(List.of("FromDisk", "FromCircle"));
 
       return brakeDiskUncertainties == 2 && circleUncertainties == 2 && specsPresent;
+    }));
+  }
+
+  @Test
+  void biDirectionalUncertaintyPropagationBetweenBrakeDiskAndCircleSameUncertaintyNotAutomaticallyCreated(
+      @TempDir Path tempDir) {
+    VirtualModel vsum = createDefaultVirtualModel(tempDir);
+    addBrakesystem(vsum, tempDir);
+    addBrakeDiscWithDiameter(vsum, tempDir, 120);
+    addUncertaintyAnnotationRepository(vsum, tempDir);
+
+    // STEP 1: Add Uncertainty to the Circle (should propagate to BrakeDisk)
+    CommittableView view1 = getDefaultView(vsum,
+        List.of(UncertaintyAnnotationRepository.class, CADRepository.class, Brakesystem.class))
+        .withChangeDerivingTrait();
+    modifyView(view1, (CommittableView v) -> {
+      var circle = v.getRootObjects(CADRepository.class).iterator().next().getCadElements().stream()
+          .filter(Circle.class::isInstance).map(Circle.class::cast)
+          .filter(c -> c.getRadius() == 60)
+          .findFirst().orElseThrow();
+
+      var uncertaintyLocation = UncertaintyFactory.eINSTANCE.createUncertaintyLocation();
+      uncertaintyLocation.setLocation(UncertaintyLocationType.PARAMETER);
+      uncertaintyLocation.setSpecification("FromCircle");
+      uncertaintyLocation.getReferencesComponents().add(circle);
+
+      var uncertainty = UncertaintyFactory.eINSTANCE.createUncertainty();
+      uncertainty.setUncertaintyLocation(uncertaintyLocation);
+      uncertainty.setKind(UncertaintyKind.MEASUREMENT_UNCERTAINTY);
+      uncertainty.setReducability(ReducabilityLevel.UNKNOWN);
+      uncertainty.setNature(UncertaintyNature.ALEATORY);
+
+      // Make sure something changes to trigger propagation
+      circle.setIdentifier(generateRandomString());
+
+      v.getRootObjects(UncertaintyAnnotationRepository.class).iterator().next().getUncertainties().add(uncertainty);
+    });
+
+    // STEP 2: Add a different Uncertainty to the BrakeDisk (should propagate to
+    // Circle)
+    CommittableView view2 = getDefaultView(vsum, List.of(UncertaintyAnnotationRepository.class, Brakesystem.class))
+        .withChangeDerivingTrait();
+    modifyView(view2, (CommittableView v) -> {
+      var brakeDisk = v.getRootObjects(Brakesystem.class).iterator().next().getBrakeComponents().stream()
+          .filter(BrakeDisk.class::isInstance).map(BrakeDisk.class::cast)
+          .filter(d -> d.getDiameterInMM() == 120)
+          .findFirst().orElseThrow();
+
+      var uncertaintyLocation = UncertaintyFactory.eINSTANCE.createUncertaintyLocation();
+      uncertaintyLocation.setLocation(UncertaintyLocationType.PARAMETER);
+      uncertaintyLocation.setSpecification("FromDisk");
+      uncertaintyLocation.getReferencesComponents().add(brakeDisk);
+
+      var uncertainty = UncertaintyFactory.eINSTANCE.createUncertainty();
+      uncertainty.setUncertaintyLocation(uncertaintyLocation);
+      uncertainty.setKind(UncertaintyKind.MEASUREMENT_UNCERTAINTY);
+      uncertainty.setReducability(ReducabilityLevel.UNKNOWN);
+      uncertainty.setNature(UncertaintyNature.ALEATORY);
+
+      // Trigger propagation
+      brakeDisk.setSpecificationType(generateRandomString());
+
+      v.getRootObjects(UncertaintyAnnotationRepository.class).iterator().next().getUncertainties().add(uncertainty);
+    });
+
+    // FINAL ASSERTION: The brakedisc has two uncertainties, one automatically
+    // added, one manually added.
+    // The circle has only one, as the parameters are equal => No new uncertainty
+    // added automatically.
+    // Attention: uncertaintyLocation is currently not part of the equal check.
+    // uncertainties
+    Assertions.assertTrue(assertView(getDefaultView(vsum, List.of(UncertaintyAnnotationRepository.class)), (View v) -> {
+      var uncertainties = v.getRootObjects(UncertaintyAnnotationRepository.class).iterator().next().getUncertainties();
+      System.out.println("Number of Uncertainties: " + uncertainties.size());
+
+      long brakeDiskUncertainties = uncertainties.stream()
+          .filter(u -> u.getUncertaintyLocation().getReferencesComponents().stream()
+              .anyMatch(c -> c instanceof BrakeDisk && ((BrakeDisk) c).getDiameterInMM() == 120))
+          .count();
+      System.out.println("brakeDiskUncertainties: " + brakeDiskUncertainties);
+
+      long circleUncertainties = uncertainties.stream()
+          .filter(u -> u.getUncertaintyLocation().getReferencesComponents().stream()
+              .anyMatch(c -> c instanceof Circle && ((Circle) c).getRadius() == 60))
+          .count();
+      System.out.println("circleUncertainties: " + circleUncertainties);
+
+      boolean specsPresent = uncertainties.stream()
+          .filter(u -> u.getUncertaintyLocation().getReferencesComponents().stream()
+              .anyMatch(c -> (c instanceof BrakeDisk && ((BrakeDisk) c).getDiameterInMM() == 120) ||
+                  (c instanceof Circle && ((Circle) c).getRadius() == 60)))
+          .map(u -> u.getUncertaintyLocation().getSpecification())
+          .collect(java.util.stream.Collectors.toSet())
+          .containsAll(List.of("FromDisk", "FromCircle"));
+
+      return brakeDiskUncertainties == 2 && circleUncertainties == 1 && specsPresent;
     }));
   }
 
