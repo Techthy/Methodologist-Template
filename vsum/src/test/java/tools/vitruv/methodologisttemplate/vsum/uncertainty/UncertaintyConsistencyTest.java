0f4f70f1756f337336ac19404ba041b649a1c71a
Claus Hammann <mail@chammann.dev>
2025-05-29 21:38:03 +0200

    refactor: update uncertainty location specification handling and add location change reactions

--- vsum/src/test/java/tools/vitruv/methodologisttemplate/vsum/uncertainty/UncertaintyConsistencyTest.java
+++ vsum/src/test/java/tools/vitruv/methodologisttemplate/vsum/uncertainty/UncertaintyConsistencyTest.java
@@ -11,6 +11,7 @@ import org.eclipse.emf.ecore.util.EcoreUtil;
 import org.eclipse.emf.ecore.xmi.impl.XMIResourceFactoryImpl;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.io.TempDir;
 import org.slf4j.Logger;
@@ -31,6 +32,7 @@ import uncertainty.Uncertainty;
 import uncertainty.UncertaintyAnnotationRepository;
 import uncertainty.UncertaintyKind;
 import uncertainty.UncertaintyLocation;
+import uncertainty.UncertaintyLocationType;
 import uncertainty.UncertaintyNature;
 import uncertainty.UncertaintyPerspective;
 import uncertainty.UncertaintyPerspectiveType;
@@ -143,11 +145,6 @@ public class UncertaintyConsistencyTest {
                         (View v) -> {
                             List<Uncertainty> uncertainties = v.getRootObjects(UncertaintyAnnotationRepository.class)
                                     .iterator().next().getUncertainties();
-                            logger.info("Uncertainties: {}", uncertainties);
-                            // LOG the primitive attributes of the uncertainties
-                            uncertainties
-                                    .forEach(u -> logger.info("Kind: {}, Reducability: {}, Nature: {}, OnDelete: {}",
-                                            u.getKind(), u.getReducability(), u.getNature(), u.getOnDelete()));
                             return uncertainties.size() == 2 && uncertainties.stream()
                                     .allMatch(u -> u.getKind() == UncertaintyKind.BELIEF_UNCERTAINTY
                                             && u.getReducability() == ReducabilityLevel.IRREDUCIBLE
@@ -418,6 +415,97 @@ public class UncertaintyConsistencyTest {
                                                     .getStochasticity() == StochasticityEffectType.PROBABILISTIC);
 
                         }));
+    }
+
+    @Disabled
+    @Test
+    void changeLocationTest(@TempDir Path tempDir) {
+
+        VirtualModel vsum = UncertaintyTestUtil.createDefaultVirtualModel(tempDir);
+        // Registers a Brakesystem, CADRepository and UncertaintyAnnotationRepository
+        UncertaintyTestUtil.registerRootObjects(vsum, tempDir);
+
+        // Add a BrakeDisk that in turn (by reactions) creates a Circle
+        UncertaintyTestUtil.addBrakeDiscWithDiameter(vsum, tempDir, 120);
+
+        // Add two uncertainties to the brake disk
+        CommittableView brakeAndUncertaintyView = UncertaintyTestUtil.getDefaultView(vsum,
+                List.of(UncertaintyAnnotationRepository.class, Brakesystem.class))
+                .withChangeDerivingTrait();
+        modifyView(brakeAndUncertaintyView, (CommittableView v) -> {
+            BrakeDisk brakeDisk = v.getRootObjects(Brakesystem.class).iterator().next().getBrakeComponents()
+                    .stream()
+                    .filter(BrakeDisk.class::isInstance).map(BrakeDisk.class::cast)
+                    .filter(d -> d.getDiameterInMM() == 120)
+                    .findFirst().orElseThrow();
+
+            UncertaintyLocation uncertaintyLocation = UncertaintyTestFactory
+                    .createUncertaintyLocation(List.of(brakeDisk));
+            uncertaintyLocation.setSpecification("FromDisk");
+            uncertaintyLocation.setLocation(UncertaintyLocationType.ANALYSIS);
+
+            Uncertainty uncertainty = UncertaintyTestFactory.createUncertainty(Optional.of(uncertaintyLocation));
+
+            v.getRootObjects(UncertaintyAnnotationRepository.class).iterator().next()
+                    .getUncertainties().add(uncertainty);
+
+            // Trigger propagation
+            brakeDisk.setSpecificationType(EcoreUtil.generateUUID());
+
+        });
+
+        // Assert that two uncertainties now exist both having the same location
+        Assertions.assertTrue(
+                assertView(
+                        UncertaintyTestUtil.getDefaultView(vsum,
+                                List.of(UncertaintyAnnotationRepository.class, Brakesystem.class)),
+                        (View v) -> {
+                            List<Uncertainty> uncertainties = v.getRootObjects(UncertaintyAnnotationRepository.class)
+                                    .iterator().next().getUncertainties();
+                            return uncertainties.size() == 2 && uncertainties.stream()
+                                    .allMatch(u -> u.getUncertaintyLocation().getSpecification().equals("FromDisk")
+                                            && u.getUncertaintyLocation()
+                                                    .getLocation() == UncertaintyLocationType.ANALYSIS);
+
+                        }));
+
+        // Change the location of the first uncertainty to DECISION_MAKING
+        modifyView(UncertaintyTestUtil
+                .getDefaultView(vsum, List.of(UncertaintyAnnotationRepository.class, Brakesystem.class))
+                .withChangeDerivingTrait(), (CommittableView v) -> {
+                    List<Uncertainty> uncertainties = v.getRootObjects(UncertaintyAnnotationRepository.class)
+                            .iterator().next().getUncertainties();
+                    Uncertainty firstUncertainty = uncertainties.get(0);
+                    UncertaintyLocation uncertaintyLocation = firstUncertainty.getUncertaintyLocation();
+                    uncertaintyLocation.setLocation(UncertaintyLocationType.DECISION_MAKING);
+                });
+
+        // Change the specification of the first uncertainty to "specificationTwo"
+        modifyView(UncertaintyTestUtil.getDefaultView(vsum, List.of(UncertaintyAnnotationRepository.class))
+
+                .withChangeDerivingTrait(), (CommittableView v) -> {
+                    List<Uncertainty> uncertainties = v.getRootObjects(UncertaintyAnnotationRepository.class)
+                            .iterator().next().getUncertainties();
+                    Uncertainty firstUncertainty = uncertainties.get(0);
+                    UncertaintyLocation uncertaintyLocation = firstUncertainty.getUncertaintyLocation();
+                    uncertaintyLocation.setSpecification("specificationTwo");
+                });
+
+        // Assert that both uncertainties now have the location DECISION_MAKING and the
+        // specification "specificationTwo"
+        Assertions.assertTrue(
+                assertView(UncertaintyTestUtil.getDefaultView(vsum, List.of(UncertaintyAnnotationRepository.class)),
+                        (View v) -> {
+                            List<Uncertainty> uncertainties = v.getRootObjects(UncertaintyAnnotationRepository.class)
+                                    .iterator().next().getUncertainties();
+                            return uncertainties.size() == 2 && uncertainties.stream()
+                                    .allMatch(u -> u.getUncertaintyLocation().getLocation()
+                                            .equals(UncertaintyLocationType.DECISION_MAKING)
+                                            && u.getUncertaintyLocation().getSpecification()
+                                                    .equals("specificationTwo"));
+
+                        }));
+
     }
 
     // These functions are only for convience, as they make the code a bit better
